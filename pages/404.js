123ef7eae27078eddd0478f490269d7a71f6cd54
tarneaux <contact.tarneo@gmail.com>
2022-06-09 19:36:12 +0200

    Add title to pages

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,15 @@
+import Head from "next/head"
+
 export default function Page404() {
   return (
-    <div style={{textAlign: "center"}}>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </div>
+    <>
+      <Head>
+        <title>page not found</title>
+      </Head>
+      <div style={{textAlign: "center"}}>
+        <h1>404</h1>
+        <p>Page not found</p>
+      </div>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Footer from '../components/footer'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>tarneo&apos;s landing page</title>
+      </Head>
       <h3>Hello, I&apos;m tarneo!</h3>
       <p>I am a college student and computer science enthusiast. I just make any project that can be useful to me, like <a href="github.com/boa-mc/">boamc</a>.</p>
       <b>Also me:</b>
