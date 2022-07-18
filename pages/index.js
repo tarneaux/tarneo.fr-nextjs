c086b0c2840bb8aabfceb43233121ef143801660
tarneo <tarneo@tarneo.fr>
2022-07-18 02:34:02 +0200

    Add neat little icons

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,9 @@ export default function Home() {
       <p>I am a college student and computer science enthusiast. I just make any project that can be useful to me, like <a href="github.com/boa-mc/">boamc</a>.</p>
       <b>Also me:</b>
       <ul>
-        <li><b>e-mail:</b> tarneo@tarneo.fr</li>
-        <li><b>Github:</b> <a href="https://github.com/tarneaux">tarneaux</a></li>
-        <li><b>Gitlab:</b> <a href="https://gitlab.com/tarneo">tarneo</a></li>
-        <li><b>discord</b>: tarneo#1560</li>
+        <li><b> e-mail:</b> tarneo@tarneo.fr</li>
+        <li><b> Github:</b> <a href="https://github.com/tarneaux">tarneaux</a></li>
+        <li><b> discord</b>: tarneo#1560</li>
       </ul>
       <Footer />
     </>
