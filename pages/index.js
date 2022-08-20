a31b1d3597ecb0301d391d615c9c4cb3974c37ee
tarneo <tarneo@tarneo.fr>
2022-08-20 19:51:15 +0200

    wait for the font to load before showing page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>tarneo&apos;s landing page</title>
         <meta name="description" content="tarneo&apos;s landing page" />
+        <link rel="preload" href="/Hasklug.otf" as="font" type="font/otf" crossOrigin="anonymous" />
       </Head>
       <h3>Hello, I&apos;m tarneo!</h3>
       <p>I am a college student and computer science enthusiast. I just make any project that can be useful to me, like <a href="https://github.com/boa-mc/">boamc</a>.</p>
